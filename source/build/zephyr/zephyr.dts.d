59b32c39334e4323f4fd3e0721806b9bffa94998
ShubhamKeshari1 <shubhamK@acmecpu.com>
2024-01-19 10:25:42 +0530

    got adc readings

---
+++ source/build/zephyr/zephyr.dts.d
@@ -0,0 +1,25 @@
+empty_file.o: /home/shubham/zephyrproject/zephyr/misc/empty_file.c \
+ /home/shubham/zephyrproject/zephyr/boards/arm/stm32f3_disco/stm32f3_disco.dts \
+ /home/shubham/zephyrproject/zephyr/dts/arm/st/f3/stm32f303Xc.dtsi \
+ /home/shubham/zephyrproject/zephyr/dts/common/mem.h \
+ /home/shubham/zephyrproject/zephyr/dts/arm/st/f3/stm32f303.dtsi \
+ /home/shubham/zephyrproject/zephyr/dts/arm/st/f3/stm32f3.dtsi \
+ /home/shubham/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/shubham/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/clock/stm32f3_clock.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/stm32_pwm.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/dma/stm32_dma.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/reset/stm32f0_1_3_reset.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/reset/stm32-common.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/shubham/zephyrproject/zephyr/dts/common/freq.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/stm32l4_adc.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/stm32_adc.h \
+ /home/shubham/zephyrproject/modules/hal/stm32/dts/st/f3/stm32f303v(b-c)tx-pinctrl.dtsi \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/stm32-pinctrl-common.h \
+ /home/shubham/zephyrproject/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /home/shubham/precision_adc/source/boards/stm32f3_disco.overlay
